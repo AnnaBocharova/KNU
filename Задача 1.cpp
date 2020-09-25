08175efa8bf001c46a5c1f0bd692f809aa5d4475
Anna <71901088+AnnaBocharova@users.noreply.github.com>
2020-09-26 02:45:03 +0300

    Add files via upload

---
+++ Задача 1.cpp
@@ -0,0 +1,21 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int x, y;
+	cout << "Enter the roots of the equation:";
+	cin >> x;
+	cin >> y;
+	int b, c;
+	b = -(x + y);
+	cout << "Сoefficient1 of the equation: " << b << endl;
+	c = x * y;
+	cout << "Coefficient2 of the equation: " << c << endl;
+	cin.get();
+	return 0;
+
+}
+
