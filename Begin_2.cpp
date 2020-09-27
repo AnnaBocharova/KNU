15e794572fc75de63527e6381afcbcb34c67fce0
Anna <71901088+AnnaBocharova@users.noreply.github.com>
2020-09-27 11:59:52 +0300

    Add files via upload

---
+++ Begin_2.cpp
@@ -0,0 +1,13 @@
+﻿// Дана сторона квадрата a. Найти его площадь S = a2
+#include <iostream>
+int main() {
+	int a, b, S;
+	b = 2;
+	std::cout << "Enter the side of the square: ";
+	std::cin >> a;
+	std::cout << "S = " << pow(a, b) << std::endl;
+	std:: cin.get();
+	return 0;
+
+}
+// S=a*a;
