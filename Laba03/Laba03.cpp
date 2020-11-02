8d58b79455690c94cafb05fc9d849e614761e7d0
Anna <71901088+AnnaBocharova@users.noreply.github.com>
2020-11-02 09:51:23 +0200

    Add files via upload

---
+++ Laba03/Laba03.cpp
@@ -0,0 +1,60 @@
+﻿#include<iostream>
+#include<cmath>
+using namespace std;
+
+
+int main() {
+
+	int n = 1000;
+	int* denum = new int[n];
+	int* num = new int[n];
+	int* factor = new int[n];
+	cout << "Enter the numenator:";
+	cin >> num[0];
+	cout << "Enter the denumenator:";
+	cin >> denum[0];
+	cout << "[";
+	factor[0] = 1;
+	if (denum[0] * num[0] > 0) {
+		for (int i = 0; i < n; i++) {
+			if (denum[i] == 0) {
+				cout << "]";
+				break;
+			}
+			else {
+				factor[i] = num[i] / denum[i];
+				cout << factor[i] << ", ";
+				denum[i + 1] = num[i] - denum[i] * factor[i];
+				num[i + 1] = denum[i];
+
+			}
+
+		}
+	}
+	else {
+		for (int i = 0; i < n; i++) {
+			if (denum[i] == 0) {
+				cout << "]";
+				break;
+			}
+			else {
+				if (num[i] < 0 || denum[i] < 0) {
+					factor[i] = (num[i] / denum[i]) - 1;
+				}
+				else {
+					factor[i] = (num[i] / denum[i]);
+				}
+				cout << factor[i] << ", ";
+				denum[i + 1] =abs( (abs(factor[i]) * abs(denum[i])) - abs(num[i]));
+				num[i + 1] = abs(denum[i]);
+
+			}
+		}
+
+		delete[] num;
+		delete[] denum;
+		delete[] factor;
+
+		return 0;
+	}
+}
---
+++ Laba03/МНК.cpp
@@ -0,0 +1,62 @@
+﻿#include<iostream>
+#include<cmath>
+#include<ctime>
+using namespace std;
+int function(float x) {
+    float function = 2 *pow( x,2) - 4;
+    return function;
+}
+int main() {
+    srand(time(NULL));
+    int k;
+    cout << "If you want to approximate the given function enter 1 , if you want to enter points enter 2:";
+    cin >> k;
+    int n;
+    float x, y;
+
+    cout << "How many points do you want to enter?";
+    cin >> n;
+    float* arrayX = new float[n];
+    float* arrayY = new float[n];
+    if (k == 1) {
+      
+        for (int i = 0; i < n; i++) {
+            x = rand() % 120;
+            arrayX[i] = x;
+            arrayY[i] = function(x);
+            cout << "{" << arrayX[i] << "," << arrayY[i] << "}" << ", ";
+         }
+        cout<<endl;
+    }
+   
+    if (k == 2) {
+        for (int i = 0; i < n; i++) {
+            cin >> x;
+            arrayX[i] = x;
+            cin >> y;
+            arrayY[i] = y;
+
+        }
+    }
+    float sumx = 0, sumy = 0, sumx2 = 0, sumxy = 0;
+    for (int i = 0; i < n; i++) {
+        sumx += arrayX[i];
+        sumy += arrayY[i];
+        sumx2 += pow(arrayX[i],2);
+        sumxy += arrayX[i]*arrayY[i];
+    }
+    float a = (n * sumxy - (sumx * sumy)) / (n * sumx2 - (pow(sumx,2)));
+    float b = (sumy - a * sumx) / n;
+    if (b > 0)
+        cout << "y=" << a << "x" << "+" << b << endl;
+    else
+        cout << "y=" << a << "x" << b << endl;
+
+    delete [] arrayX;
+    delete[] arrayY;
+    return 0;
+}
+
+
+
+
