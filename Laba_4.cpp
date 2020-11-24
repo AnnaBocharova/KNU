612fc83e8cda3c70d912171c5d47fd0a82847c14
Anna <71901088+AnnaBocharova@users.noreply.github.com>
2020-11-24 05:01:18 +0200

    Add files via upload

---
+++ Laba_4.cpp
@@ -0,0 +1,96 @@
+﻿#include<iostream>
+#include "Laba_4.h"
+
+
+using namespace std;
+// Дано рядок. Дописати до нього мінімальну кількість символів так, щоб він став паліндромом.
+
+	
+int findMinSymbol(char str[], int l, int h)
+{
+   
+    if (l == h) return 0;
+    if (l == h - 1) return (str[l] == str[h]) ? 0 : 1;
+
+    if (str[l] == str[h])
+        return findMinSymbol(str, l + 1, h - 1);
+    else
+        return  (min(findMinSymbol(str, l, h - 1), findMinSymbol(str, l + 1, h)) + 1);
+        
+}
+int main()
+{
+    char str[] = "edef";
+    int n = strlen(str);
+  // cout << findMinSymbol(str, 0, strlen(str) - 1);
+    int l = n + findMinSymbol(str, 0, strlen(str) - 1);
+    
+    char newstr[100] = "";
+    
+    int f = l-1;
+    int k = 0;
+    for (int i = n - 1; i > 0; i--) {
+
+        char tmp = str[i];
+        newstr[k] = tmp;
+        newstr[l - k - 1] = tmp;
+        k++;
+    }
+    cout << newstr << endl;
+    int counter = 0;
+   bool Ispolindrom=true;
+    for (int h = 0; h < n; h++) {
+        for (int s = 0; s < l; s++) {
+            if (str[h] == newstr[s]) {
+                counter++;
+            }
+        }
+            if (counter == 0) {
+                Ispolindrom = false;
+
+            }
+            else
+                counter = 0;
+        
+    }
+
+
+
+    char newstr2[100] = "";
+    for (int i = 0; i < n; i++) {
+
+        char tmp = str[i];
+        newstr2[f] = tmp;
+        newstr2[i] = tmp;
+        f--;
+
+    }
+    cout << newstr2 << endl;
+    int counter2 = 0;
+    bool Ispolindrom2=true;
+    for (int h = 0; h < n; h++) {
+        for (int s = 0; s < l; s++) {
+            if (str[h] == newstr2[s]) {
+                counter2++;
+            }
+        }
+            if (counter2 == 0) {
+                Ispolindrom2 = false;
+
+            }
+            else
+                counter2 = 0;
+        
+    }
+    if (Ispolindrom==false)
+        cout << newstr2;
+    else if(Ispolindrom2==false)
+        cout << newstr;
+     
+    
+    
+    
+  
+    return 0;
+}
+
---
+++ лаба4.cpp
@@ -0,0 +1,86 @@
+﻿#include<iostream>
+#include <cmath>
+#include "лаба4.h"
+using namespace std;
+int triangle(int a, int b, int c) {
+	double angle0 = acos((b * b + c * c - a * a) / (2 * b * c));
+	double angle1 = acos((c * c + a * a - b * b) / (2 * a * c));
+	double angle2 = acos((a * a + b * b - c * c) / (2 * a * b));
+	double maxAngle = angle0 > angle1 ? (angle0 > angle2 ? angle0 : angle2) : (angle1 > angle2 ? angle1 : angle2);
+	if (maxAngle < 3.15 / 2)
+		return 0;
+	else
+		return 1;
+	
+}
+
+void sortarray(int size, int  array1[9], int& counter, int  array[10], int& k, int newsize, int& counter2)
+{
+	for (int i = 0; i < size; i++) {
+		for (int j = 0; j < size; j++) {
+			if (array1[i] == array1[j]) {
+				counter++;
+			}
+		}
+		if (counter <= 3) {
+			int value = array1[i];
+			array[k] = value;
+			k++;
+			counter = -1;
+
+		}
+		else {
+			for (int l = 0; l < newsize; l++) {
+				if (array1[i] == array[l]) {
+					counter2++;
+				}
+			}
+			if (counter2 < 3) {
+				array[k] = array1[i];
+				counter2 = 0;
+				k++;
+				counter = -1;
+			}
+			else {
+				counter2 = 0;
+				counter = -1;
+			}
+		}
+
+	}
+}
+
+int main() {
+	//Дано масив натуральних чисел. Вивести на екран без повторень всі трійки чисел
+	//з масиву, які є сторонами гострокутного трикутника
+	int array1[] = {1,1,1,6,6,3,1,1,1};
+	int array[10];
+	int counter = -1,counter2=0, k=0;
+	int size = (sizeof(array1) / sizeof(array1[0]));
+	int newsize = (sizeof(array) / sizeof(array[0]));
+	sortarray(size, array1, counter, array, k, newsize, counter2);
+	int l = 0;
+	int tmp = k;
+	int newarray[100];
+	for (int i = 0; i < tmp; i++) {
+		for (int j = 0; j < tmp; j++) {
+			if (j+i+1 >= tmp)break;
+			int k = triangle(array[i], array[i+1], array[j+i+2]);
+			if (k == 0) {
+				newarray[l] = array[i];
+				newarray[l + 1] = array[i+1];
+				newarray[l + 2] = array[j+i+2];
+				l += 3;
+			}
+
+		}
+	}
+	for (int i = 0; i < l; i+=3) {
+		cout << "{";
+		cout << newarray[i] << ",";
+		cout << newarray[i + 1] << ",";
+		cout<< newarray[i + 2];
+		cout << "}";
+	}
+	return 0;
+}
